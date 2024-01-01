142cdb6b101d926ebd655904ff8191c0f19386a3
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-01-01 15:14:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-      /* add your code here */
+        char[][][] elements = new char[2][2][2];
+
+        System.out.println("Number of layers: " + elements.length);
+        System.out.println("Number of rows: " + elements[0].length);
+        System.out.println("Number of columns: " + elements[0][0].length);
+
+        System.out.println("Initial array:");
+        printArray(elements);
+
+        elements[1][0][1] = 'A';
+
+        System.out.println("Array after updating:");
+        printArray(elements);
     }
 
+    private static void printArray(char[][][] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Turn " + i + ":");
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    System.out.print(array[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
