9cf30f42f606f20e1d068565345fc703b3f5d7c9
KhalidDev <148249284+devAlq@users.noreply.github.com>
2024-01-08 13:52:18 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
       int[][][] array = new int[2][2][2];
-      int layer = array.length;
       int col = array[0][0].length;
-      int row = array[0][1].length;
+      int layer = array.length;
+      int row = array[0].length;
       for (int i = 0; i < layer; i++) {
         for (int a = 0; a < col; a++) {
             for (int b = 0; b <row; b++) {
